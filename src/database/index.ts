ff8c4fe51e8e1fc2ce7de854fc2d007c52db392d
Ariful Alam <alamariful1727@gmail.com>
2024-06-02 15:19:06 -0400

    feat: singleton pattern for a PostgreSQL

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,22 +6,36 @@ import log from "./../utils/logger";
 import { IDatabaseConfig } from "./types";
 
 class Database {
-	pool: Pool;
+	private static instance: Pool;
 
-	constructor() {
-		const poolConfig = config.get<IDatabaseConfig>("db");
-		this.pool = new Pool(poolConfig);
-		log.info("PostgreSQL: Pool created successfully.");
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	private constructor() {}
+
+	public static getInstance(): Pool {
+		if (!Database.instance) {
+			const poolConfig = config.get<IDatabaseConfig>("db");
+			Database.instance = new Pool(poolConfig);
+			log.info("PostgreSQL: Pool created successfully.");
+
+			Database.instance.on("error", (err: Error) => {
+				log.error(err, "PostgreSQL: Unexpected error on idle client");
+				process.exit(-1);
+			});
+		}
+
+		return Database.instance;
 	}
 
-	runMigrations = async (): Promise<void> => {
+	public static runMigrations = async (): Promise<void> => {
+		const client = await Database.getInstance().connect();
 		try {
-			const client = await this.pool.connect();
 			log.info("PostgreSQL: Client created successfully.");
 			await migrate({ client }, path.resolve(__dirname, "migrations/sql"));
 			log.info("PostgreSQL: Migration completed.");
 		} catch (err) {
 			log.error(err, "PostgreSQL: Migration failed");
+		} finally {
+			client.release();
 		}
 	};
 }
